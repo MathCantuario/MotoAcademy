1bf171f83484fa72d9fad6057095dd0fc2e0651a
Matheus de C Cantuario <matheuscantuario@gmail.com>
2024-07-15 19:12:39 -0400

    Changes

---
+++ Javascript/aula01.js
@@ -0,0 +1,27 @@
+const variavel1 = 1; //number
+const string = "texto"; //string
+const booleano = true; // boolean true ou false
+const nulo = null;
+const undefined; 
+
+const objeto = {
+    nome: "Euclides"
+    idade: 22
+}
+objeto.nome = "Linton"
+
+const array = ["teste",212130, function true]
+
+let variavel2 = 'asdasd';
+fuction TextDecoderStream() {
+    variavel2 = 'Mudou';
+    TextDecoderStream(variavel2);
+}
+functionteste2(variavel) {
+    variavel2 = "mudou 2"
+    variavel = "mudou 3";
+}
+ teste();
+ console.log(variavel2);
+
+ 
